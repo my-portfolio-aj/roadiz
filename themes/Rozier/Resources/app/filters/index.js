91df1ef97da39f5ac28562c45decc69e8c1a9924
Adrien Scholaert <adrien@rezo-zero.com>
2018-02-23 11:50:50 +0100

    Added  Translator (bazinga-translator) as common filter

--- themes/Rozier/Resources/app/filters/index.js
+++ themes/Rozier/Resources/app/filters/index.js
@@ -1,9 +1,11 @@
 import centralTruncate from './centralTruncate'
 import truncate from './truncate'
 import formatDate from './formatDate'
+import trans from './trans'
 
 export default {
     centralTruncate,
     truncate,
-    formatDate
+    formatDate,
+    trans
 }
